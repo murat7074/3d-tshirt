5ce1a2c14430a44ffd8ae3a063dc9967dace53f0
murat7074 <muratyonev@gmail.com>
2023-09-30 18:17:06 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import { App as Canvas } from "./Canvas";
+import Overlay from "./Overlay";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <>
+    <Canvas />
+    <Overlay />
+  </>
+);
